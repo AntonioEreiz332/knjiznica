8caf52a8f2bf32da2b843671a6c587e69703434b
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2022-02-07 09:25:10 +0100

    Add files via upload

--- appzabiblioteku/appzabiblioteku/UnosKnjiga.Designer.cs
+++ appzabiblioteku/appzabiblioteku/UnosKnjiga.Designer.cs
@@ -36,6 +36,7 @@ namespace appzabiblioteku
             this.txtbox_nazivKnjige = new System.Windows.Forms.TextBox();
             this.lbl_naziv = new System.Windows.Forms.Label();
             this.btn_izlaz = new System.Windows.Forms.Button();
+            this.btn_Unesi = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtbox_isbnKnjige
@@ -96,11 +97,22 @@ namespace appzabiblioteku
             this.btn_izlaz.UseVisualStyleBackColor = true;
             this.btn_izlaz.Click += new System.EventHandler(this.btn_izlaz_Click);
             // 
+            // btn_Unesi
+            // 
+            this.btn_Unesi.Location = new System.Drawing.Point(179, 270);
+            this.btn_Unesi.Name = "btn_Unesi";
+            this.btn_Unesi.Size = new System.Drawing.Size(75, 23);
+            this.btn_Unesi.TabIndex = 14;
+            this.btn_Unesi.Text = "Unesi";
+            this.btn_Unesi.UseVisualStyleBackColor = true;
+            this.btn_Unesi.Click += new System.EventHandler(this.btn_Unesi_Click);
+            // 
             // frm_unosKnjiga
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(594, 397);
+            this.Controls.Add(this.btn_Unesi);
             this.Controls.Add(this.btn_izlaz);
             this.Controls.Add(this.txtbox_isbnKnjige);
             this.Controls.Add(this.lbl_isbn);
@@ -124,5 +136,6 @@ namespace appzabiblioteku
         private System.Windows.Forms.TextBox txtbox_nazivKnjige;
         private System.Windows.Forms.Label lbl_naziv;
         private System.Windows.Forms.Button btn_izlaz;
+        private System.Windows.Forms.Button btn_Unesi;
     }
 }
--- appzabiblioteku/appzabiblioteku/UnosKnjiga.cs
+++ appzabiblioteku/appzabiblioteku/UnosKnjiga.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
 
 namespace appzabiblioteku
 {
     public partial class frm_unosKnjiga : Form
     {
+        List<knjige> knjige = new List<knjige>();
+        static string putdokumenta = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        static string xmldokument = ("Knjige.xml");
+        static string path = Path.Combine(putdokumenta, xmldokument);
         public frm_unosKnjiga()
         {
             InitializeComponent();
@@ -20,6 +26,59 @@ namespace appzabiblioteku
         private void btn_izlaz_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btn_Unesi_Click(object sender, EventArgs e)
+        {
+            try {
+                knjige Knjiga = new knjige(Convert.ToInt32(txtbox_isbnKnjige.Text), txtbox_nazivKnjige.Text, txtbox_autorKnjige.Text);
+                knjige.Add(Knjiga);
+                DialogResult result;
+
+                txtbox_autorKnjige.Clear();
+                txtbox_isbnKnjige.Clear();
+                txtbox_nazivKnjige.Clear();
+
+                result = MessageBox.Show("Želite li upisati još knjiga?", "Upozorenje", MessageBoxButtons.YesNo);
+                if(result == DialogResult.No)
+                {
+                    try
+                    {
+                        var Knjige = XDocument.Load(path);
+                        foreach (knjige knjiga in knjige)
+                        {
+                            var knjigax = new XElement("Knjiga",
+                                new XElement("ISBN", knjiga.Id_isbn),
+                                new XElement("Autor", knjiga.Autor),
+                                new XElement("Naziv", knjiga.Naziv));
+                            Knjige.Root.Add(knjigax);
+                        }
+                        Knjige.Save(path);
+
+                    }catch(Exception gr)
+                    {
+                        var Knjige = new XDocument();
+                        var rootelem = new XElement("Knjige");
+                        Knjige.Add(rootelem);
+                        foreach (knjige knjiga in knjige)
+                        {
+                            var knjigax = new XElement("Knjiga",
+                                new XElement("ISBN", knjiga.Id_isbn),
+                                new XElement("Autor", knjiga.Autor),
+                                new XElement("Naziv", knjiga.Naziv));
+                            Knjige.Root.Add(knjigax);
+                        }
+                        Knjige.Save(path);
+                    }
+                    knjige.Clear();
+                    this.Close();
+                }
+            
+            }catch(Exception ae)
+            {
+                MessageBox.Show(ae.ToString(), "Greška", MessageBoxButtons.OK);
+
+            }
         }
     }
 }
